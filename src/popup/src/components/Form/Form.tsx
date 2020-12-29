5bca1d50b675b66093f7830e290a79ef377c59ad
Márcio Santos <marcio.tbms@gmail.com>
2020-12-29 15:47:42 -0300

    Focus on the input when opening the popup

--- src/popup/src/components/Form/Form.tsx
+++ src/popup/src/components/Form/Form.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { FormContainer, ScanButton, TextField } from './styles';
 
 const Header: React.FC = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (inputRef.current) inputRef.current.focus();
+  }, []);
+
   return (
     <FormContainer>
-      <TextField />
+      <TextField ref={inputRef} />
       <ScanButton>Scan</ScanButton>
     </FormContainer>
   );
